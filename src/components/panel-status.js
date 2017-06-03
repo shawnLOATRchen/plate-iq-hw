f120d9fba861adcca478eb3b7a5f894a43f4824b
Shawn Chen <ethanloatrchen@gmail.com>
2017-06-03 16:48:38 -0700

    finish panel status

--- src/components/panel-status.js
+++ src/components/panel-status.js
@@ -2,19 +2,48 @@ import React, {Component} from 'react'
 
 
 export default class PanelStatus extends Component{
-
+  statusLeft(){
+    switch (this.props.progress) {
+      case "explorted":
+        return "APPROVED"
+      case "paid":
+        return "PENDING EXPORT"
+      case "direct":
+      default:
+        return "NEEDS REVISION"
+    }
+  }
+  statusRight(){
+    switch (this.props.progress) {
+      case "explorted":
+        return [
+          <i className="fa fa-check" aria-hidden="true" key="right1"></i>,
+          <span key="right2"> 4 item(s) require account mapping </span>
+        ]
+      case "paid":
+        return [
+          <i className="fa fa-arrow-right" aria-hidden="true" key="right1"></i>,
+          <span key="right2">This invoice is pending export</span>
+        ]
+      case "direct":
+      default:
+        return [
+          <i className="fa fa-exclamation-triangle" aria-hidden="true" key="right1"></i>,
+          <span key="right2"> 4 item(s) require account mapping </span>
+        ]
+    }
+  }
   render(){
     return(
-      <div className="panel-status d-flex">
+      <div className={"panel-status d-flex " + (this.props.progress === "direct" ? "red":"green")}>
         <div className="status-left">
           <i className="fa fa-chevron-left" aria-hidden="true"></i>
           <span> 12/33 </span>
           <i className="fa fa-chevron-right" aria-hidden="true"></i>
-          <span> NEEDS REVISION </span>
+          <span className="progress-name">{this.statusLeft()}</span>
         </div>
         <div className="status-right">
-          <i className="fa fa-exclamation-triangle" aria-hidden="true"></i>
-          <span> 4 item(s) require account mapping </span>
+          {this.statusRight()}
         </div>
 
       </div>
