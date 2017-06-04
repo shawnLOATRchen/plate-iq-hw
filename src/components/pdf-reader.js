93c76458a4df27eb65fad112d117789f83a546bb
Shawn Chen <ethanloatrchen@gmail.com>
2017-06-03 21:51:22 -0700

    finish pdf reader control panel

--- src/components/pdf-reader.js
+++ src/components/pdf-reader.js
@@ -5,9 +5,51 @@ export default class PDFReader extends Component{
   render(){
     return(
       <div className="pdf-reader">
-        <object data="invoice.pdf" type="application/pdf" width="100%" height="100%">
-          <p>Alternative text - include a link <a href="myfile.pdf">to the PDF!</a></p>
-        </object>
+        <div className="pdf-container">
+          <object data="invoice.pdf" type="application/pdf">
+            <p>Alternative text - include a link <a href="myfile.pdf">to the PDF!</a></p>
+          </object>
+        </div>
+        <div className="control-panel row">
+          <div className="zoom col-5 d-flex justify-content-between align-items-center">
+            <i className="fa fa-search-minus" aria-hidden="true"></i>
+            <div className="bar"/>
+            <div className="circle"/>
+            <i className="fa fa-search-plus" aria-hidden="true"></i>
+          </div>
+          <div className="page col-2 d-flex justify-content-around align-items-center">
+            <i className="fa fa-chevron-left" aria-hidden="true"></i>
+            <span> 1/3 </span>
+            <i className="fa fa-chevron-right" aria-hidden="true"></i>
+          </div>
+          <div className="btns col-5 d-flex justify-content-around align-items-center">
+            <div className="btn btn-secondary"><i className="fa fa-download" aria-hidden="true"></i> Download PDF</div>
+            <div className="dropdown">
+              <button className="btn btn-secondary dropdown-toggle" type="button" id="pdfReaderDropDown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Actions
+              </button>
+              <div className="dropdown-menu" aria-labelledby="pdfReaderDropDown">
+                <div className="dropdown-item d-flex justify-content-between align-items-center trash" href="#">
+                  <span>Delete invoice image</span>
+                  <i className="fa fa-trash" aria-hidden="true"></i>
+                </div>
+                <div className="dropdown-item d-flex justify-content-between align-items-center" href="#">
+                  <span>upload new image</span>
+                  <i className="fa fa-upload" aria-hidden="true"></i>
+                </div>
+                <hr/>
+                <div className="dropdown-item d-flex justify-content-between align-items-center" href="#">
+                  <span>Fullscreen</span>
+                  <i className="fa fa-arrows-alt" aria-hidden="true"></i>
+                </div>
+                <div className="dropdown-item d-flex justify-content-between align-items-center" href="#">
+                  <span>Rotate 90</span>
+                  <i className="fa fa-undo" aria-hidden="true"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
