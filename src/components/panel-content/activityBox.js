c86d4153b6343cfdcd9633ff0f61fb3bd2562ac6
Shawn Chen <ethanloatrchen@gmail.com>
2017-06-03 20:03:04 -0700

    finish history

--- src/components/panel-content/activityBox.js
+++ src/components/panel-content/activityBox.js
@@ -1,21 +1,49 @@
 import React, {Component} from 'react'
 
 export default class ActivityBox extends Component{
+  getStatus(){
+    if (this.props.item.approved) return " blue";
+    else if (this.props.item.flagged) return " red";
+    else return ""
+  }
 
+  getRightBtn(){
+    if (this.props.item.flagged){
+      if (this.props.item.resolved) {
+        return (
+          <button className="btn resolved" type="button">
+            Resolved
+          </button>
+        )
+      } else {
+        return (
+          <button className="btn to-resolve" type="button">
+            Resolve Flag
+          </button>
+        )
+      }
+    }
+  }
   render(){
     const item = this.props.item;
     return(
       <div className="activity-box row">
-        <div className="col-3">{item.date}</div>
+        <div className="col-3 d-flex flex-column">
+          <div className="date">{item.date}</div>
+          <div className="time">{item.time}</div>
+        </div>
         <div className="col-6 d-flex">
-          <div className="activity-status d-flex flex-column align-items-center">
+          <div className="activity-status align-items-center">
             <div className="status-top"></div>
-            <div className="status-circle"></div>
+            <div className={"status-circle" + this.getStatus()}></div>
             <div className="status-buttom"></div>
           </div>
-          <div className="content">{item.title}</div>
+          <div className="content d-flex flex-column justify-content-center">
+            <div className={"title" + this.getStatus()}>{item.title}</div>
+            {item.discription.length > 0 && <div className="discription">{item.discription}</div>}
+          </div>
         </div>
-        <div className="col-3 text-right">{item.resolved}</div>
+        <div className="col-3 d-flex flex-column justify-content-center">{this.getRightBtn()}</div>
       </div>
     )
   }
--- src/components/panel-content/history.js
+++ src/components/panel-content/history.js
@@ -11,7 +11,10 @@ export default class AccountSplits extends Component{
   render(){
     return(
       <div className="history">
-        <h6>Recent Activity</h6>
+        <div className="d-flex justify-content-between align-items-center">
+          <h6>Recent Activity</h6>
+          <div className="btn btn-secondary btn-flag"><i className="fa fa-flag" aria-hidden="true"></i> Flag Invoice</div>
+        </div>
         {historyItem.map((item, index) => {
           return(
             <ActivityBox key={index} item={item}/>
